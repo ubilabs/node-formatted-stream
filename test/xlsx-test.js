8402df923d7aba21b6517d92f4a093c8fa9c6a7b
Keno Schwalb <keno@kenoschwalb.com>
2016-08-04 10:57:29 +0200

    test(xlsx): verify basic reading/writing

--- test/xlsx-test.js
+++ test/xlsx-test.js
@@ -1,6 +1,9 @@
+/* eslint-disable max-nested-callbacks */
 import {expect} from 'chai';
 import stream from 'stream';
 import xlsx from '../src/components/xlsx';
+import fs from 'fs';
+import path from 'path';
 
 describe('The XLSX component', () => {
   it('should export a `createReadStream` function', () => {
@@ -15,11 +18,34 @@ describe('The XLSX component', () => {
     it('should return a transform stream', () => {
       expect(xlsx.createReadStream({})).to.be.instanceof(stream.Transform);
     });
+
+    it('should push objects when lines are written', done => {
+      const xlsxStream = xlsx.createReadStream({}),
+        expectedResult = {key1: 'value1', key2: 'value2'},
+        testFilePath = path.join(__dirname, 'misc/testobject.xlsx');
+
+      xlsxStream.on('data', data => expect(data).to.deep.equal(expectedResult));
+      xlsxStream.on('end', done);
+      fs.createReadStream(testFilePath).pipe(xlsxStream);
+    });
   });
 
   describe('.createWriteStream', () => {
     it('should return a transform stream', () => {
       expect(xlsx.createWriteStream({})).to.be.instanceof(stream.Transform);
+    });
+
+    it('should push lines when objects are written', done => {
+      const xlsxStream = xlsx.createWriteStream({}),
+        xlsxReadStream = xlsx.createReadStream({}),
+        object = {key1: 'value1', key2: 'value2'};
+
+      xlsxReadStream.on('end', done);
+      xlsxReadStream.on('data', data => expect(data).to.deep.equal(object));
+
+      xlsxStream.pipe(xlsxReadStream);
+      xlsxStream.write(object);
+      xlsxStream.end();
     });
   });
 });
