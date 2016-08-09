fdcfdd1cc0f8a0a6d259a71e02d800616cd93381
Keno Schwalb <keno@kenoschwalb.com>
2016-08-09 11:39:39 +0200

    refactor(util): add DocBlock and rename var

--- src/util.js
+++ src/util.js
@@ -2,28 +2,52 @@
 import stream from 'stream';
 
 const _mapFunction = new WeakMap(),
-  _streamObject = new WeakMap();
+  _inputStream = new WeakMap();
 
+/**
+ * Array.map for streams.
+ **/
 export class StreamMap extends stream.Transform {
-  constructor(mapFunction, streamObject) {
+  /**
+   * Create an instance of the StreamMap class.
+   * @param {Function} mapFunction Map function which will be applied
+   *                               to each item in the stream
+   * @param {Object} inputStream The stream on which the mapFunction will
+   *                             be applied to
+   **/
+  constructor(mapFunction, inputStream) {
     super({objectMode: true});
     _mapFunction.set(this, mapFunction);
-    _streamObject.set(this, streamObject);
+    _inputStream.set(this, inputStream);
 
-    streamObject.on('data', data => this.push(data));
+    inputStream.on('data', data => this.push(data));
   }
 
+  /**
+   * Automatically called by stream.Transform for
+   * every item written to this stream.
+   * Applies the map function and pushes the result.
+   * @param {Object} data Data written to the stream
+   * @param {string} encoding Encoding, if data is a String
+   * @param {Function} done Callback function
+   **/
   _transform(data, encoding, done) {
     const mapFunction = _mapFunction.get(this),
-      streamObject = _streamObject.get(this);
+      inputStream = _inputStream.get(this);
 
-    streamObject.write(mapFunction ? mapFunction(data) : data);
+    inputStream.write(mapFunction ? mapFunction(data) : data);
     done();
   }
 
+  /**
+   * Automatically called by stream.Transform when no more items will be
+   * written to this stream.
+   * Simply passed on to the inputStream.
+   * @param {Function} done Callback function
+   **/
   _flush(done) {
-    const streamObject = _streamObject.get(this);
-    streamObject.on('end', done);
-    streamObject.end();
+    const inputStream = _inputStream.get(this);
+    inputStream.on('end', done);
+    inputStream.end();
   }
 }
